c7060e075dc2437ea4e272841d30204a2a23026b
VaasuDevanS <vaasuceg.96@gmail.com>
2024-10-12 11:35:00 -0300

    added header image, pie chart

--- survey.py
+++ survey.py
@@ -9,35 +9,50 @@ References:
 import re
 from collections import Counter
 
+import altair as alt
+import pandas as pd
 import streamlit as st
 import yaml
 from streamlit_gsheets import GSheetsConnection
 
 
 st.set_page_config(page_title='Ayurveda Doshas Survey')
-st.title('Ayurveda Doshas Survey')
+
+st.markdown(
+    "<h1 style='text-align: center;'>Ayurveda Doshas Survey</h1>",
+    unsafe_allow_html=True,
+)
+st.image('header.jpg')
 
 with st.form('survey_form'):
     with open('questions.yml', 'r') as file:
         questions = yaml.safe_load(file)
 
     responses = {
-        'name': st.text_input('Your Name:'),
+        'name': st.text_input('Name:red[*]', placeholder='Enter your name'),
         'answers': {},
     }
     for ix, ques in enumerate(questions['questions'], start=1):
         ques_label, options = ques.popitem()
         options = [f'{v} ({k})' for k, v in options.items() if v is not None]
         responses['answers'][ix] = st.radio(f'{ix}.) {ques_label}', options, index=None)
-    submitted = st.form_submit_button('Submit your responses')
+
+    def disable_button():
+        st.session_state.submitted = True
+
+    if 'submitted' not in st.session_state:
+        st.session_state.submitted = False
+    submitted = st.form_submit_button(
+        'Submit your response',
+        on_click=disable_button,
+        disabled=st.session_state.submitted,
+    )
 
     if submitted:
-
         if not responses['name']:
             st.error('Please enter your name')
 
         else:
-
             # Stats
             answers = [r for r in responses['answers'].values() if r is not None]
             categories = [re.findall(r'\((.*?)\)', answer)[0] for answer in answers]
@@ -60,3 +75,17 @@ with st.form('survey_form'):
             st.success(f'Thanks {responses["name"]} for completing the survey!')
             st.write(f'You answered {len(answers)} out of 29 questions')
             st.write(cat_counts)
+
+            cat_counts_df = pd.DataFrame(
+                cat_counts.items(), columns=['Doshas', '#Questions']
+            )
+            pie_chart = (
+                alt.Chart(cat_counts_df)
+                .mark_arc()
+                .encode(
+                    theta=alt.Theta(field='#Questions', type='quantitative'),
+                    color=alt.Color(field='Doshas', type='nominal'),
+                    tooltip=['Doshas', '#Questions'],
+                )
+            )
+            st.altair_chart(pie_chart, use_container_width=True)
