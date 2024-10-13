fa1751c2918aa424cd5859ace134f7a1ec12a615
VaasuDevanS <vaasuceg.96@gmail.com>
2024-10-13 15:43:20 -0300

    hide category from survey, fix YAML yes issue

--- survey.py
+++ survey.py
@@ -1,12 +1,11 @@
 """
 Author: Vaasudevan Srinivsan <vaasuceg.96@gmail.com>
 Created: Oct 10, 2024
-Modified: Oct 12, 2024
+Modified: Oct 13, 2024
 References:
     - https://docs.streamlit.io/develop/tutorials/databases/private-gsheet
 """
 
-import re
 from collections import Counter
 from datetime import datetime
 
@@ -33,10 +32,11 @@ with st.form('survey_form'):
         'name': st.text_input('Name:red[*]', placeholder='Enter your name'),
         'answers': {},
     }
+    answer_keys = {}
     for ix, ques in enumerate(questions['questions'], start=1):
         ques_label, options = ques.popitem()
-        options = [f'{v} ({k})' for k, v in options.items() if v is not None]
-        responses['answers'][ix] = st.radio(f'{ix}.) {ques_label}', options, index=None)
+        answer_keys[ix] = options = {v: k for k, v in options.items() if v is not None}
+        responses['answers'][ix] = st.radio(f'{ix}.) {ques_label}', options.keys(), index=None)
 
     def disable_button():
         st.session_state.submitted = True
@@ -55,8 +55,14 @@ with st.form('survey_form'):
 
         else:
             # Stats
-            answers = [r for r in responses['answers'].values() if r is not None]
-            categories = [re.findall(r'\((.*?)\)', answer)[0] for answer in answers]
+            categories = []
+            answers_with_cat = []
+            for ix, answer in responses['answers'].items():
+                if answer is not None:
+                    categories.append(answer_keys[ix][answer])
+                    answers_with_cat.append(f'{answer} ({categories[-1]})')
+                else:
+                    answers_with_cat.append(None)
             cat_counts = Counter(categories)
 
             # Save to Google Sheet
@@ -65,17 +71,17 @@ with st.form('survey_form'):
             df.loc[len(df.index)] = [
                 datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                 responses['name'],
-                len(answers),
+                len(categories),
                 cat_counts['Pitta'],
                 cat_counts['Vata'],
                 cat_counts['Kapha'],
-                *responses['answers'].values(),
+                *answers_with_cat,
             ]
             conn.update(worksheet='Responses', data=df)
             st.cache_data.clear()
 
             st.success(f'Thanks "{responses["name"]}" for completing the survey!')
-            st.write(f'You answered {len(answers)} out of 29 questions')
+            st.write(f'You answered {len(categories)} out of 29 questions')
             st.write(cat_counts)
 
             cat_counts_df = pd.DataFrame(
